337adc2784291984130f984dab23b2032072807f
Sjaak Luthart <mail@sjaakluthart.nl>
2016-03-21 10:29:35 +0100

    renamed file

---
+++ src/react/app.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Home = require('./pages/home.jsx');
+
+ReactDOM.render(<Home />, document.getElementById('app'));
--- src/react/main.jsx
+++
@@ -1,5 +0,0 @@
-var React = require('react');
-var HomePage = require('./pages/home').HomePage;
-
-var mountNode = document.getElementById("react-main-mount");
-React.renderComponent(new HomePage({ size: 200 }), mountNode);
