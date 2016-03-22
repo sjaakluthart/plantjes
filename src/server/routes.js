4dd12cf975fd7420d272fcc1c3d8a6581c5a8f04
Sjaak Luthart <mail@sjaakluthart.nl>
2016-03-22 20:58:48 +0100

    added mongo connection and test route

--- src/server/routes.js
+++ src/server/routes.js
@@ -5,12 +5,29 @@ var HomePage = require('../react/pages/home.jsx');
 
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
 router.get('/', function(req, res) {
-
   var component = ReactDOMServer.renderToString(<Layout><HomePage /></Layout>);
 
   res.render('layout', {react: component});
+});
+
+var url = 'mongodb://localhost:27017/plantjes';
+
+// Test route for getting data in React component
+router.get('/test', function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+    console.log('Connected to server.');
+
+    db.collection('documents').find().toArray(function(err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+      res.send(result);
+    });
+  });
 });
 
 module.exports = router;
--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 require('node-jsx').install();
 
 var router = require('./routes.js');
@@ -8,6 +9,7 @@ var app = express();
 var config = {
   port: 3000
 };
+var url = 'mongodb://localhost:27017/plantjes';
 
 // Views
 app.set('views', __dirname, 'layout.ejs');
@@ -23,3 +25,15 @@ app.use('/', router);
 app.listen(config.port, function (err) {
   console.log('Server started; listening on port ' + config.port);
 });
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
