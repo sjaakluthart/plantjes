0b038a3a1ddb1602a4edb19e670a5e024b9b2df7
Sjaak Luthart <mail@sjaakluthart.nl>
2016-03-24 10:14:07 +0100

    removed ssr

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,17 +1,6 @@
-var React = require('react');
-var ReactDOMServer = require('react-dom/server');
-var Layout = require('../react/layout.jsx');
-var HomePage = require('../react/pages/home.jsx');
-
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-
-router.get('/', function(req, res) {
-  var component = ReactDOMServer.renderToString(<Layout><HomePage /></Layout>);
-
-  res.render('layout', {react: component});
-});
 
 var url = 'mongodb://localhost:27017/plantjes';
 
