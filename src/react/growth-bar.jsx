a3183b7da7855971d4af2e541383a80824f5d213
Sjaak Luthart <mail@sjaakluthart.nl>
2016-03-28 14:17:29 +0200

    added growth-bar

---
+++ src/react/growth-bar.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import moment from 'moment'
+
+const GrowthBar = React.createClass({
+  displayName: 'GrowthBar',
+
+  propTypes: {
+    plantedOn: React.PropTypes.string.isRequired,
+    plantIcon: React.PropTypes.string.isRequired,
+    harvestOn: React.PropTypes.string.isRequired
+  },
+
+  getDuration() {
+    let start = moment(this.props.plantedOn);
+    let end = moment(this.props.harvestOn);
+
+    return end.diff(start, 'days');
+  },
+
+  getPosition() {
+    let start = moment(this.props.plantedOn);
+    let today = moment();
+
+    return today.diff(start, 'days');
+  },
+
+  calculateGrowthPercentage() {
+    let duration = this.getDuration();
+    let position = this.getPosition();
+    let percentage = Math.ceil((position / duration) * 100);
+
+    return {
+      width: `${percentage}%`
+    }
+  },
+
+  render() {
+    return (
+      <div  className="growth-bar">
+        <h1>Groei:<span>{this.getPosition()}/{this.getDuration()} dagen</span></h1>
+        <section>
+          <div style={this.calculateGrowthPercentage()}></div>
+          <img src="assets/seed.svg" />
+          <img src={this.props.plantIcon} />
+        </section>
+      </div>
+    );
+  }
+});
+
+export {GrowthBar}
--- src/react/plant-info.jsx
+++ src/react/plant-info.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import moment from 'moment'
 
+import {GrowthBar} from './growth-bar.jsx'
+
 const PlantInfo = React.createClass({
   displayName: 'PlantInfo',
 
   render() {
     let src = `assets/${this.props.plant.species}.jpg`;
+    let plantIcon = `assets/${this.props.plant.species}.svg`;
     return (
       <section className="plant-info">
         <img className="plant-picture" src={src} />
-        <p><strong>Geplant op:</strong> {moment(this.props.plant.plantedOn).format('DD-MM-YYYY')}</p>
+        <GrowthBar plantedOn={this.props.plant.plantedOn} harvestOn={this.props.plant.harvestOn} plantIcon={plantIcon}/>
       </section>
     );
   }
