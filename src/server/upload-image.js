24e0e7b145c23a37938455ef098b57ec294e9d73
Sjaak Luthart <mail@sjaakluthart.nl>
2016-03-29 15:02:50 +0200

    removed spaces

--- src/server/upload-image.js
+++ src/server/upload-image.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: 'dist/uploads/' });
+var upload = multer({dest: 'dist/uploads/'});
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 
