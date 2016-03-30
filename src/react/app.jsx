0b3ac9cf85d4a4a8027ac7adbd422f1e3c0e4364
Sjaak Luthart <mail@sjaakluthart.nl>
2016-03-30 20:38:38 +0200

    added routes with routie

--- src/react/app.jsx
+++ src/react/app.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import {Layout} from './layout.jsx'
-import {Home} from './home.jsx'
+import {PlantList} from './plant-list.jsx'
+import {Plant} from './plant.jsx'
 
-ReactDOM.render(<Layout><Home /></Layout>, document.getElementById('app'));
+routie({
+  ''() {
+    ReactDOM.render(
+      <Layout>
+        <PlantList />
+      </Layout>,
+      document.getElementById('app')
+    );
+  },
+
+  'plant/:plantId'(plantId) {
+    ReactDOM.render(
+      <Layout>
+        <Plant plantId={plantId} />
+      </Layout>,
+      document.getElementById('app')
+    );
+  }
+});
