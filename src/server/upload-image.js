c4d6aa034658afdb838af3d33f124651a76bd4cd
Sjaak Luthart <mail@sjaakluthart.nl>
2016-03-30 20:41:09 +0200

    re arranged requires, changed var name

--- src/server/upload-image.js
+++ src/server/upload-image.js
@@ -1,15 +1,16 @@
 var express = require('express');
-var router = express.Router();
 var multer = require('multer');
-var upload = multer({dest: 'dist/uploads/'});
-var MongoClient = require('mongodb').MongoClient;
+var Mongo = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
+
+var router = express.Router();
+var upload = multer({dest: 'dist/uploads/'});
 
 var url = 'mongodb://localhost:27017/plantjes';
 
 router.post('/', upload.single('file'), function(req, res){
   // Update the Plant with a new image, send the image name to the client on success.
-  MongoClient.connect(url, function(err, db) {
+  Mongo.connect(url, function(err, db) {
     db.collection('plants').updateOne({_id: ObjectId(req.body.plantId)}, {$set: {plantPicture: req.file.filename}}, function(err, result) {
       if (err) {
         throw err;
