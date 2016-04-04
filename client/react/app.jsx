42a57aa34d4722093a239fca91a601ebc400f203
Sjaak Luthart <mail@sjaakluthart.nl>
2016-04-04 18:20:52 +0200

    changed fonts

--- client/react/app.jsx
+++ client/react/app.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Layout from './layout.jsx';
 import PlantList from './plant-list.jsx';
 import Plant from './plant.jsx';
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 ReactDOM.render((
   <Router history={browserHistory}>
@@ -13,24 +20,4 @@ ReactDOM.render((
       <Route path="/plant/:plantId" component={Plant}/>
     </Route>
   </Router>
-), document.getElementById('app'))
-
-// routie({
-//   ''() {
-//     ReactDOM.render(
-//       <Layout>
-//         <PlantList />
-//       </Layout>,
-//       document.getElementById('app')
-//     );
-//   },
-//
-//   'plant/:plantId'(plantId) {
-//     ReactDOM.render(
-//       <Layout>
-//         <Plant plantId={plantId} />
-//       </Layout>,
-//       document.getElementById('app')
-//     );
-//   }
-// });
+), document.getElementById('app'));
