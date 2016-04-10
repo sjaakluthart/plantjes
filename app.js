bf1a2665f26792d1de8222728cb338c8de43bd80
Sjaak Luthart <mail@sjaakluthart.nl>
2016-04-10 17:37:20 +0200

    Removed promise, changed to es6 syntax.

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 // Import Node Modules
-var express = require('express');
-var path = require('path');
-var Promise = require('bluebird');
-var winston = require('winston');
+const express = require('express');
+const path = require('path');
+const winston = require('winston');
 
 // Import Routes
-var plantList = require('./server/routes/plant-list.js');
-var plantData = require('./server/routes/plant-data.js');
-var uploadImage = require('./server/routes/upload-image.js');
+const plantList = require('./server/routes/plant-list.js');
+const plantData = require('./server/routes/plant-data.js');
+const uploadImage = require('./server/routes/upload-image.js');
 
 // Import Functions
-var db = require('./server/db.js');
-var insertPlant = require('./server/insert-plant.js');
+const db = require('./server/db.js');
+const insertPlant = require('./server/insert-plant.js');
 
 // Express setup
-var app = express();
+const app = express();
 
-var config = {
+const config = {
   port: 3000
 };
-var url = 'mongodb://localhost:27017/plantjes';
+const url = 'mongodb://localhost:27017/plantjes';
 
 app.use('/plant-list', plantList);
 app.use('/plant', plantData);
@@ -29,30 +28,28 @@ app.use('/upload', uploadImage);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Send all requests to index.html so browserHistory in React Router works.
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-// Connect to Mongo
-Promise.promisify(db.connect)(url)
-.then(function() {
-  // Start the server
-  return app.listen(config.port);
-})
-.then(function() {
-  // Log on success
-  winston.log('info', 'Connected to Mongo. Express server started, listening on port %s.', config.port);
-
-  // Check if there are plants in the collection.
-  return db.get().collection('plants').find().toArray();
-})
-.then(function(result) {
-  // Insert plants if there are none.
-  winston.log('info', 'Found %s plants in collection.', result.length);
-  if (result.length === 0) {
-    insertPlant();
+db.connect(url, (err) => {
+  if (err) {
+    winston.log('error', err);
+    return false;
   }
-})
-.catch(function(err) {
-  winston.log('error', err);
+
+  app.listen(config.port);
+  winston.log(
+    'info',
+    'Connected to Mongo. Express server started, listening on port %s.',
+    config.port
+  );
+
+  db.get().collection('plants').find().toArray((err, res) => {
+    // Insert plants if there are none.
+    winston.log('info', 'Found %s plants in collection.', res.length);
+    if (res.length === 0) {
+      insertPlant();
+    }
+  });
 });
