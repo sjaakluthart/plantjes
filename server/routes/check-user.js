752366e006086fb296670102794efa5604953444
Sjaak Luthart <mail@sjaakluthart.nl>
2016-04-23 16:00:28 +0200

    Added checkUser route.

---
+++ server/routes/check-user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const winston = require('winston');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  if (req.session.user) {
+    res.send({ authorised: true, user: req.session.user });
+  } else {
+    res.send({ authorised: false, user: null });
+  }
+});
+
+module.exports = router;
