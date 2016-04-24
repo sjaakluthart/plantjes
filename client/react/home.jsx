9279b49338696a5e89e6a88c8ca7c533e8a8e958
Sjaak Luthart <mail@sjaakluthart.nl>
2016-04-24 12:40:06 +0200

    Added check for logged in user.

--- client/react/home.jsx
+++ client/react/home.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
+import $ from 'jquery';
 
 import text from './text.json';
 
 import { Paper } from 'material-ui';
-import { Link } from 'react-router';
+import { browserHistory, Link } from 'react-router';
 
-function Home() {
-  return (
-    <section className="home">
-      <h1>{text.appTitle}</h1>
-      <h2>
-        {text.homeTitle}
-      </h2>
-      <h3>
-        {text.homeSubtitle}
-      </h3>
-      <Paper style={{ borderRadius: '10px' }} className="app-icon" zDepth={2}>
-        <img src="/assets/app-icon.svg" alt="app-icon" />
-      </Paper>
-      <Link className="button-start" to={'/register'}>
-        aan de slag
-      </Link>
-      <Link className="login" to={'/login'}>
-        ik heb al een account
-      </Link>
-    </section>
-  );
+class Home extends React.Component {
+  componentWillMount() {
+    $.ajax({ url: '/check-user' })
+    .then((data) => {
+      if (data.authorised) {
+        browserHistory.push('/plants');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <section className="home">
+        <h1>{text.appTitle}</h1>
+        <h2>
+          {text.homeTitle}
+        </h2>
+        <h3>
+          {text.homeSubtitle}
+        </h3>
+        <Paper style={{ borderRadius: '10px' }} className="app-icon" zDepth={2}>
+          <img src="/assets/app-icon.svg" alt="app-icon" />
+        </Paper>
+        <Link className="button-start" to={'/register'}>
+          aan de slag
+        </Link>
+        <Link className="login" to={'/login'}>
+          ik heb al een account
+        </Link>
+      </section>
+    );
+  }
 }
 
 export default Home;
