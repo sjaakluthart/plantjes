c91c9684d428e1a9241734e403b9b65f70111d75
Sjaak Luthart <mail@sjaakluthart.nl>
2016-04-28 13:24:16 +0200

    Added db connection and call to readSensor with test values.

--- server/schedule.js
+++ server/schedule.js
@@ -1,19 +1,36 @@
+const db = require('./db.js');
 const schedule = require('node-schedule');
 const readSensor = require('./sensor.js');
 const winston = require('winston');
 
-// TODO check the user's plant data every hour or so, send message if their plant requires
-// attention
+const url = 'mongodb://localhost:27017/plantjes';
 
-winston.log('info', 'Starting schedule every 2secs');
+winston.log('info', 'Starting schedule every 10 secs');
 
-// Module from https://www.npmjs.com/package/node-schedule
-schedule.scheduleJob('*/10 * * * * *', () => {
-  winston.log('info', 'I get called every 10 seconds!');
-  readSensor(0);
-  // Find all plants in the collection
-  // Connect to the Raspberry Pi, read sensor value
-  // Save sensor value in DB for each plants
-  // Decide if the plant needs attention
-  // Send message if attention is needed
+db.connect(url, (err) => {
+  if (err) {
+    winston.log('error', err);
+    return false;
+  }
+
+  // Module from https://www.npmjs.com/package/node-schedule
+  schedule.scheduleJob('*/10 * * * * *', () => {
+    winston.log('info', 'I get called every 10 seconds!');
+    // Find all plants in the collection
+    // Connect to the Raspberry Pi, read sensor value
+    // Save sensor value in DB for each plants
+    // Decide if the plant needs attention
+    // Send message if attention is needed
+    db.get().collection('plants').findOne(
+      { species: 'sla' },
+      (err, res) => {
+        if (err) {
+          throw err;
+        }
+        winston.log('info', 'Found plant species: %s.', 'sla');
+        winston.log('info', res.sensors.moisture, res.referenceValues.moisture);
+        readSensor(res.sensors.moisture, res.referenceValues.moisture);
+      }
+    );
+  });
 });
