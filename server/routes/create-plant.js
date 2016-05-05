0b58f2ed9dca75dd5116c5fdb59a7082a846ea95
Sjaak Luthart <mail@sjaakluthart.nl>
2016-05-05 11:32:09 +0200

    Changed reference value to array with objects.

--- server/routes/create-plant.js
+++ server/routes/create-plant.js
@@ -26,20 +26,23 @@ router.post('/', (req, response) => {
       light: req.body.light,
       temperture: req.body.temperture
     },
-    referenceValues: {
-      temperature: {
+    referenceValues: [
+      {
+        type: 'temperture',
         min: 7,
         max: 24
       },
-      moisture: {
+      {
+        type: 'moisture',
         min: 30,
         max: 5
       },
-      light: {
+      {
+        type: 'light',
         min: 77,
         max: 25
       }
-    },
+    ],
     sensorReadings: [
       {
         moisture: 10,
