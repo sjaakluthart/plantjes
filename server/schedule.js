d9ec4fa8c4912c4e83e5807ab885014bd9612fcc
Sjaak Luthart <mail@sjaakluthart.nl>
2016-05-05 12:48:03 +0200

    Added test with db data.

--- server/schedule.js
+++ server/schedule.js
@@ -5,7 +5,7 @@ const winston = require('winston');
 
 const url = 'mongodb://localhost:27017/plantjes';
 
-winston.log('info', 'Starting schedule every 10 secs');
+winston.log('info', 'Starting schedule every 5 secs');
 
 db.connect(url, (err) => {
   if (err) {
@@ -14,22 +14,35 @@ db.connect(url, (err) => {
   }
 
   // Module from https://www.npmjs.com/package/node-schedule
-  schedule.scheduleJob('*/10 * * * * *', () => {
-    winston.log('info', 'I get called every 10 seconds!');
+  schedule.scheduleJob('*/5 * * * * *', () => {
+    winston.log('info', 'I get called every 5 seconds!');
     // Find all plants in the collection
     // Connect to the Raspberry Pi, read sensor value
     // Save sensor value in DB for each plants
     // Decide if the plant needs attention
     // Send message if attention is needed
     db.get().collection('plants').findOne(
-      { species: 'sla' },
+      {},
       (err, res) => {
         if (err) {
           throw err;
         }
-        winston.log('info', 'Found plant species: %s.', 'sla');
-        winston.log('info', res.sensors.moisture, res.referenceValues.moisture);
+        winston.log('info', 'Found plant species: %s.', res.species);
+        winston.log('info', res.sensors, res.referenceValues);
+        winston.log('info', 'moisture channel %s', res.sensors.moisture);
+        winston.log('info', 'light channel %s', res.sensors.light);
+        winston.log('info', 'temperature channel %s', res.sensors.temperture);
+
         readSensor(res.sensors.moisture, res.referenceValues.moisture);
+
+        setTimeout(() => {
+          readSensor(res.sensors.light, res.referenceValues.light);
+        }, 3000);
+
+        // setTimeout(() => {
+        //   readSensor(2)
+        // }, 6000);
+        // readSensor(res.sensors.moisture, res.referenceValues.moisture);
       }
     );
   });
